6f55ed75ef331ce069f05ae93b53d54bcf9459a2
Yat Fung (Michael)Kwan <michael370662@gmail.com>
2021-10-25 00:02:26 +0800

    Initial Commit

---
+++ Shared/MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacApp.swift
+//  Shared
+//
+//  Created by apple on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
